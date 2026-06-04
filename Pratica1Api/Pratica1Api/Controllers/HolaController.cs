05dedfb06ef18664665a7a9cf7ca2af30a3cc2d8
Jusorpht234 <Jusorpht234@DESKTOP-LR7HCI2>
2026-06-04 18:17:10 -0400

    Agregar archivos de proyecto.

---
+++ Pratica1Api/Pratica1Api/Controllers/HolaController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Pratica1Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HolaController : ControllerBase
+    {
+        // GET: api/<HolaController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<HolaController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<HolaController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<HolaController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<HolaController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
