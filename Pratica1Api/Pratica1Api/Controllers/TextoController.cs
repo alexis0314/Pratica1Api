29ab29adb57f6058509550943632424e62d95ae3
Alexis234 <Jusorpht234@DESKTOP-LR7HCI2>
2026-06-05 21:36:40 -0400

    .

---
+++ Pratica1Api/Pratica1Api/Controllers/TextoController.cs
@@ -0,0 +1,83 @@
+﻿ using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Pratica1Api.Controllers
+{
+    public class TextoController : Controller
+    {
+        // GET: TextoController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: TextoController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+        [HttpGet]
+        // GET: TextoController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TextoController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TextoController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: TextoController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TextoController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: TextoController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
